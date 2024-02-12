014f6e829374466a827d521b098759b8989a1781
Geethma Rathnayake <geeth4862@gmail.com>
2024-02-12 21:17:45 +0530

    added motions

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles/main.css";
+import DisplayTodos from "./components/DisplayTodos";
+import Todos from "./components/Todos";
+
+import { motion } from "framer-motion";
+function App() {
+  return (
+    <div className="App">
+      <h1 
+      >
+        Todo App
+      </h1>
+      <div>
+        <Todos />
+        <DisplayTodos />
+      </div>
+    </div>
+  );
+}
+
+export default App;
